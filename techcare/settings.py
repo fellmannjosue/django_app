2eac81c9d91741f411f602ed410c5782f59a32df
fellmanjosue <fellmannjosue12@unitec.edu>
2024-12-18 09:43:18 -0600

    update Django settings module to 'techcare.settings' in manage.py

--- techcare/settings.py
+++ techcare/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Aplicaciones personalizadas
     'helpdesk',
     'inventory',
     'maintenance',
     'accounts',
 ]
+
 
 # ---------------------------------------------------------------------------
 # ⚙️ MIDDLEWARE
